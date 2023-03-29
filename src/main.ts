916efbe0dd3f844c2a9b3215277bcd503415cd3f
alexzhu <zrljp12@gmail.com>
2023-03-29 08:51:20 +0800

    update: 添加fs和path权限

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,30 @@
-import { invoke } from "@tauri-apps/api/tauri";
+import { invoke } from "@tauri-apps/api/tauri"
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
+import { readBinaryFile } from "@tauri-apps/api/fs"
+import { resolveResource } from "@tauri-apps/api/path"
+
+let greetInputEl: HTMLInputElement | null
+let greetMsgEl: HTMLElement | null
 
 async function greet() {
   if (greetMsgEl && greetInputEl) {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     greetMsgEl.textContent = await invoke("greet", {
       name: greetInputEl.value,
-    });
+    })
+  }
+  try {
+    const resource = await resolveResource("files.zip")
+    console.log(resource)
+  } catch (err) {
+    console.error(err)
   }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
+  greetInputEl = document.querySelector("#greet-input")
+  greetMsgEl = document.querySelector("#greet-msg")
   document
     .querySelector("#greet-button")
-    ?.addEventListener("click", () => greet());
-});
+    ?.addEventListener("click", () => greet())
+})
